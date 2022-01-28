8b15bc546caaf354da6d17bdecc90ee39b411ca1
GEUN <tjrrms7707@icloud.com>
2022-01-28 11:07:27 +0900

    fixup! 테스트 커핏

---
+++ src/boj/step/chapter6/no_15596.java
@@ -0,0 +1,12 @@
+package boj.step.chapter6;
+
+public class no_15596 {
+    public static long sum(int[] a){
+        long sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+
+}
---
+++ src/boj/step/chapter6/no_4673.java
@@ -0,0 +1,19 @@
+package boj.step.chapter6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class no_4673 {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+
+
+    }
+}
